29434177dd8b97fa46ec65814acabadb5690b310
test <root@ip-172-31-90-253.ec2.internal>
2024-02-26 16:02:17 +0000

    1st commit

--- dora.sh
+++ dora.sh
@@ -2,6 +2,6 @@
 USERNAME="dedsec1327@gmail.com"
 Password="Ded@1327#"
 docker build -t doraimage .
-docker tag newimage ded1327/doraimage
+docker tag doraimage ded1327/doraimage
 echo "$Password" | docker login --username "$USERNAME" --password-stdin
 docker push ded1327/doraimage
